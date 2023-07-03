e073ba9c73c2c4d7ee0bf22bb72cfede55f596d1
Mthini <mthiniliyema58@gmail.com>
2023-07-03 07:34:05 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets in lower case only using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lttrs[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0 : i < 26 : ++i)
+	{
+		putchar(lttrs[i]);
+	}
+	putchar("\n");
+	return (0);
+}
